91e42b8f486d32d47944723b5bc563e660ba8a60
Brad Nicholes <brad.nicholes@quest.com>
2023-06-07 16:53:52 -0600

    Simple SignalR server and client to demonstrate certificate issue.

---
+++ SimpleSignalrClient/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SimpleSignalrClient
+{
+    public class Program
+    {
+        private static HubConnection _connection;
+        private static bool ignoreSsl = false; //Set to true to use a default validation callback
+        private static readonly RemoteCertificateValidationCallback _validationCallback = CertificateValidationCallback; //Set to null to use no callback
+        private static string certPath = @"<your-pfx-cert-path-here";
+        private static string certPassword = "<your-pfx-cert-passphrase-here";
+
+        public static async Task Main(string[] args)
+        {
+            var x509 = new X509Certificate2(File.ReadAllBytes(certPath), certPassword);
+
+            _connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:8443/myHub", options =>
+                {
+                    options.ClientCertificates.Add(x509);
+
+                    options.HttpMessageHandlerFactory = (message) =>
+                    {
+                        if (message is HttpClientHandler clientHandler)
+                        {
+                            if (ignoreSsl)
+                            {
+                                clientHandler.ServerCertificateCustomValidationCallback =
+                                    (sender, certificate, chain, sslPolicyErrors) => true;
+                            }
+                            else
+                            {
+                                if (_validationCallback == null)
+                                {
+                                    // Use standard validation
+                                }
+                                else
+                                {
+                                    clientHandler.ServerCertificateCustomValidationCallback =
+                                        (sender, certificate, chain, sslPolicyErrors) => _validationCallback(sender, certificate, chain, sslPolicyErrors);
+                                }
+                            }
+                        }
+                        return message;
+                    };
+                })
+                .Build();
+
+            _connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await _connection.StartAsync();
+            };
+
+            await ConnectAsync();
+
+            bool stop = false;
+            while (!stop)
+            {
+                Console.WriteLine("Press any key to send message to server and receive echo");
+                Console.ReadKey();
+                Send("testuser", "msg");
+                Console.WriteLine("Press q to quit or anything else to resume");
+                var key = Console.ReadLine();
+                if (key == "q") stop = true;
+            }
+        }
+
+        private static async Task ConnectAsync()
+        {
+            _connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                Console.WriteLine("Received message");
+                Console.WriteLine($"user: {user}");
+                Console.WriteLine($"message: {message}");
+            });
+            try
+            {
+                await _connection.StartAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e);
+            }
+        }
+
+        private static async void Send(string user, string msg)
+        {
+            try
+            {
+                await _connection.InvokeAsync("SendMessage", user, msg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e}");
+            }
+        }
+
+        public static bool CertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+
+    }
+}
---
+++ SimpleSignalrServer/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SimpleSignalrServer
+{
+    public class Program
+    {
+        private static string certPath = @"<your-pfx-cert-path-here>";
+        private static string certPassword = "<your-pfx-cert-passphrase-here>";
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel(options =>
+                    {
+                        options.Listen(IPAddress.Any, 8443, listenOptions => // https
+                        {
+                            listenOptions.UseHttps(certPath, certPassword);
+                        });
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<MyHub>("/myHub");
+            });
+        }
+    }
+}
